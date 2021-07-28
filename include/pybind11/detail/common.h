bfea868090b0520c0112539d39aaa5daa44089f6
Jerome Robert <jeromerobert@gmx.com>
2021-07-28 17:38:07 +0200

    Revert "[do not merge] disable deprecation warning"
    This reverts commit 81ca2338702941f8b6b56b7e95522fbe6534ffe2.

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -93,9 +93,9 @@
 #endif
 
 #if defined(PYBIND11_CPP14)
-#  define PYBIND11_DEPRECATED(reason)
+#  define PYBIND11_DEPRECATED(reason) [[deprecated(reason)]]
 #else
-#  define PYBIND11_DEPRECATED(reason)
+#  define PYBIND11_DEPRECATED(reason) __attribute__((deprecated(reason)))
 #endif
 
 #if defined(PYBIND11_CPP17)
